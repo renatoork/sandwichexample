7d0863adec338546b926b609f0456d6e2d412bd7
Renato Arruda <renatoork@gmail.com>
2019-05-20 01:47:17 -0300

    docker

---
+++ StartServerAPI.ps1
@@ -0,0 +1,30 @@
+Write-Output ""
+Set-Location .\Sandwish.Server
+Write-Output "Build Server"
+dotnet build
+if ($LASTEXITCODE -eq 0) {
+    Write-Output "Test Server"
+    dotnet test
+    if ($LASTEXITCODE -eq 0) {
+        Write-Output "Publish Server"
+        dotnet publish
+        if ($LASTEXITCODE -eq 0) {
+            Write-Output "Exec Server"
+            docker build -t serverapi
+            docker run -p 5001:5001 serverapi
+        } else {
+            Write-Output "Publish fail"    
+        }
+    } else {
+        Write-Output "Test fail"
+    }
+} else {
+    Write-Output "Build fail"
+}
+Write-Output ""
+Write-Output ""
+Write-Output ""
+Write-Output ""
+Write-Output ""
+Write-Output ""
+Write-Output ""
